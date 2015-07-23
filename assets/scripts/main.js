cc2a64c4b333e537a8e432a66e2615a27c531f0a
Thomas Asadurian <Tasadurian@gmail.com>
2015-07-22 18:46:18 -0700

    polling

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,7 +10,7 @@ var scream = function() {
     xhr.onreadystatechange = function(evt) {
       evt.preventDefault();
       if (xhr.status === 200 && xhr.readyState === 4) {
-        location.reload();
+        //location.reload();
       }
     };
   });
@@ -33,39 +33,24 @@ var loopJSON = function(json) {
     createTweets(json[i].Message, json[i].Username, json[i].Time);
   }
 };
-
+//////////polling
 var pollNewTweets = function() {
-  var originalLength = '';
-  var httpRequest = new XMLHttpRequest();
-  httpRequest.open('GET', '/api/tweets' + (username ? ("?username=" +
-    username) : ""), true);
-  httpRequest.setRequestHeader("Content-Type",
-    "application/json;charset=UTF-8");
-  httpRequest.send(null);
-  http.onreadystatechange = function(evt) {
-    evt.preventDefault();
-    if ((httpRequest.readyState === 4) && (httpRequest.status === 200)) {
-      var response = JSON.parse(httpRequest.responseText);
-      originalLength = response.length;
-    }
-  };
-  return originalLength;
-};
-
-var countPoll = function(poll) {
-  var originalPoll = 0;
-  var newPoll = '';
   window.setInterval(function() {
-    newPoll = originalPoll + poll;
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.open('GET', '/api/tweets');
+    httpRequest.setRequestHeader("Content-Type",
+      "application/json;charset=UTF-8");
+    httpRequest.send(null);
+    httpRequest.onreadystatechange = function(evt) {
+      evt.preventDefault();
+      if ((httpRequest.readyState === 4) && (httpRequest.status === 200)) {
+        var response = JSON.parse(httpRequest.responseText);
+        var poller = document.querySelector('#poller');
+        poller.textContent = response.length;
+      }
+    };
   }, 10000);
-  return newPoll;
 };
-
-var displayNewPoll = function(newPoll) {
-  var poller = document.querySelector('#poller');
-  poller.textContent = newPoll;
-};
-
 
 var getTweets = function() {
   var username = location.pathname.split("/").pop();
@@ -104,4 +89,4 @@ var followFunction = function() {
 scream();
 getTweets();
 followFunction();
-displayNewPoll();
+pollNewTweets();
